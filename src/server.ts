b745708f453bede8428348167ff09e283befa264
Breno Barbareli <breno_barbareli@hotmail.com>
2024-01-18 17:32:26 -0300

    create first routes

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,12 @@ import express from 'express'
 
 const app = express()
 
+app.get('/test', (request, response) => {
+  return response.send('funcionou')
+})
+
+app.post('/test-post', (request, response) => {
+  return response.send('funcionou o post')
+})
+
 app.listen(3000, () => console.log('server is running'))
