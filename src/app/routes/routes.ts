1266fa508e764477f73980c5706c1481ee189bf4
Breno Barbareli <breno_barbareli@hotmail.com>
2024-03-14 16:31:06 -0300

    create middleware

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import tagRouter from '../controller/TagController'
 import userRouter from '../controller/UserController'
+import { ensureAdmin } from '../middlewares/ensureAdmin'
 
 const routers = Router()
 
 routers.use('/users', userRouter)
-routers.use('/tags', tagRouter)
+routers.use('/tags', ensureAdmin, tagRouter)
 
 export default routers
